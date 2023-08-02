8d78a7e86fea2ad044973c5adcd6fbdd07ff57d6
René Pirringer <rene@pirringer.at>
2023-08-02 17:19:38 +0200

    added embedded player

--- EmbeddedPlayer/ViewController.swift
+++ EmbeddedPlayer/ViewController.swift
@@ -6,11 +6,48 @@
 //
 
 import UIKit
+import PinLayout
+import AVKit
 
 class ViewController: UIViewController {
+	let label = UILabel()
+	
+	var actualPreviewController: UIViewController?
+	
+	override func loadView() {
+		super.loadView()
+		self.view.backgroundColor = .systemBackground
+		
+		self.view.addSubview(label)
+		label.text = "Embedded Player Test.\nThere are no playback controls visible after the app launch. Only the video preview image is shown. When you tap the vidoe then the controls are shown."
+		label.textColor = .label
+		label.textAlignment = .center
+		label.numberOfLines = 0
+		
+		label.layout
+			.pin(.top, .leading, .trailing, gap: 44)
+		
+		if let url = URL(string: "https://ciqua.com/transfer/video/fountain.mp4") {
+			addPlayer(mediaURL: url)
+		}
+	}
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
+
+	func addPlayer(mediaURL: URL) {
+		let playerController = AVPlayerViewController()
+		playerController.showsTimecodes = true
+		playerController.player = AVPlayer(url: mediaURL)
+		playerController.beginAppearanceTransition(true, animated: false)
+		self.addChild(playerController)
+		view.addSubview(playerController.view)
+
+		playerController.view.layout
+			.pin(.top, to: label, gap: 44)
+			.pin(.bottom, .leading, .trailing)
+
+		playerController.didMove(toParent: self)
+		self.actualPreviewController = playerController
+		playerController.endAppearanceTransition()
 	}
 
 
