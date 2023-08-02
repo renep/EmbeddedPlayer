29bc6525c0e8571028ef0e350a55626e7f2f544e
René Pirringer <rene@pirringer.at>
2023-08-02 16:33:50 +0200

    initial app configured

---
+++ EmbeddedPlayer/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  EmbeddedPlayer
+//
+//  Created by René Pirringer on 02.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+
+}
+
